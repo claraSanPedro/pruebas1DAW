8ab197c5fa7aeab28952f405b9f11866616fe674
clara <clara@correo.es>
2022-03-16 20:12:48 +0100

    Versión 3, quito el comentario

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -2,8 +2,9 @@
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		System.out.println("Hola");
+		System.out.println("Te vuelvo a saludar");
 	}
 	
 }
