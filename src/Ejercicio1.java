fd750a4d88e7c8db82bee479896ca3d4104cf119
Clara <101628024+claraSanPedro@users.noreply.github.com>
2022-03-17 20:42:24 +0100

    Modificada línea Syso

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -6,7 +6,7 @@ public class Ejercicio1 {
 		System.out.println("Hola");
 		System.out.println("Te vuelvo a saludar");
 		System.out.println("Subida conectada con GitHub");
-		System.out.println("Modificaci GitHub");
+		System.out.println("Modificación GitHub 2);
 	}
 	
 }
