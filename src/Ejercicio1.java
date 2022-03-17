e3dc9ab648a269174cb35afa1d8496537201e81f
Clara <101628024+claraSanPedro@users.noreply.github.com>
2022-03-17 20:36:00 +0100

    Añado Syso desde GitHub

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -6,6 +6,7 @@ public class Ejercicio1 {
 		System.out.println("Hola");
 		System.out.println("Te vuelvo a saludar");
 		System.out.println("Subida conectada con GitHub");
+		System.out.println("Modificaci GitHub");
 	}
 	
 }
