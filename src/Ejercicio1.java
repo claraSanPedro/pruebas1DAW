751b88d9b2c2ccee531b795fbfea6e2b9ae1be76
Anton Kartashov <anton.kartashov@iessampedro.org>
2022-03-23 20:57:42 +0100

    Corregido

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -6,7 +6,7 @@ public class Ejercicio1 {
 		System.out.println("Hola");
 		System.out.println("Te vuelvo a saludar");
 		System.out.println("Subida conectada con GitHub");
-		System.out.println("Modificación GitHub 2);
+		System.out.println("Modificación GitHub 2");
 	}
 	
 }
